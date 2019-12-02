5151c0e10d7d8ce74411bef2af01c44fcb6245e7
Jan -{Pospa}- Pospisil <pospa@pospa.net>
2019-12-02 17:05:05 +0100

    KeyVaultEndpoint init

---
+++ WebAPI/Controllers/DeviceController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pospa.Azure.IoT.DeviceSimulator;
+
+namespace Pospa.Azure.IoT.DeviceSimulator.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        private AssetManagementHelper _assetManagementHelper;
+
+        public DeviceController()
+        {
+
+            _assetManagementHelper = new AssetManagementHelper()
+        }
+
+        // GET: api/Device
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Device/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(string id)
+        {
+            return "value";
+        }
+
+        // POST: api/Device
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Device/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -16,8 +19,22 @@ namespace Pospa.Azure.IoT.DeviceSimulator.WebAPI
             CreateHostBuilder(args).Build().Run();
         }
 
+        private static string GetKeyVaultEndpoint() => "https://<>.vault.azure.net";
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var keyVaultEndpoint = GetKeyVaultEndpoint();
+                    if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                    {
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback));
+                        config.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
